9ae6133e290e8f413dec30f400760da9a6078673
chenhao <694886886@qq.com>
2024-09-26 17:05:39 +0800

    🐞 fix: any

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './App.css'
 import pinyin from 'pinyin'
 
 function App() {
-  const inputRef = useRef()
-  const [data, setData] = useState([])
+  const inputRef: any = useRef()
+  const [data, setData] = useState<any>([])
   const handlePy = () => {
     console.log()
     const py = pinyin(inputRef.current.value, { style: pinyin.STYLE_NORMAL }).map(word => word[0].toUpperCase());
@@ -19,7 +19,7 @@ function App() {
         <br />
         <button onClick={handlePy}>英文</button>
         <br />
-        {data?.length && data.map(item => {
+        {data?.length && data.map((item: any) => {
           return <div>
             {item}
           </div>
